dfce487f560e82e5af7143099a49260c9207f0b3
Igor-Bertolini <bertoliniigor@gmail.com>
2024-04-26 20:23:16 -0300

    importando dados

---
+++ rdocs/Igor Bertolini.R
@@ -0,0 +1,8 @@
+# Carregando o pacote necessário
+library(readr)
+
+# Lendo os dados
+dados <- read_csv("C:/ESTAT/projeto-fantasma/banco")
+
+# Visualizando as primeiras linhas dos dados
+head(dados)
