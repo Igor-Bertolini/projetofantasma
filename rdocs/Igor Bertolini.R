f1c2b3f2312806ada22919deaf245335da1edb41
Igor-Bertolini <bertoliniigor@gmail.com>
2024-04-27 02:03:18 -0300

    grafico de variacao da nota imdb por temporada dos episodios

--- rdocs/Igor Bertolini.R
+++ rdocs/Igor Bertolini.R
@@ -2,7 +2,21 @@
 library(readr)
 
 # Lendo os dados
-dados <- read_csv("C:/ESTAT/projeto-fantasma/banco")
+setwd("C:/ESTAT/projeto-fantasma/")
+getwd()
+dados <- read.csv("C:/ESTAT/projeto-fantasma/banco/banco_final.csv")
 
 # Visualizando as primeiras linhas dos dados
 head(dados)
+library(dplyr)
+
+##1) Número de lançamentos a cada década por formato de lançamento;
+
+str(dados)
+library(ggplot2)
+library(lubridate)
+dados$date_aired <- as.Date(dados$date_aired)
+ggplot(dados, aes(x = date_aired, fill = format)) +
+  geom_histogram(position = "identity", alpha = 0.5) +
+  theme_minimal() +
+  labs(x = "Data de Exibição", y = "Contagem", fill = "Formato", title = "Número de Lançamentos por Formato ao Longo do Tempo")
